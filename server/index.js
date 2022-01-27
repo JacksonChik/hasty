f2b82cd90f82602ac597add280ac8693aac9dfda
Chik Jackson <Jacksonchik01@hotmail.com>
2022-01-27 01:37:10 -0800

    Heroku deployment stuff

--- server/index.js
+++ server/index.js
@@ -16,10 +16,9 @@ app.use('/posts', postRoutes);
 app.get('/',(req,res) => {
     res.send('Connected to hasty\'s api')
 })
-const CONNECTION_URL = 'mongodb+srv://jacksonchik01:2001_08_02@cluster0.pxltj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewURLParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewURLParser: true, useUnifiedTopology: true})
 .then(()=>app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error)=>console.log(error.message));
 
