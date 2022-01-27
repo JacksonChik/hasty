a1c57935083ec7c7c20f3ac6857867e38dba9a32
Chik Jackson <Jacksonchik01@hotmail.com>
2022-01-27 01:50:12 -0800

    Cleanup

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
@@ -21,5 +20,3 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL,{useNewURLParser: true, useUnifiedTopology: true})
 .then(()=>app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error)=>console.log(error.message));
-
-// mongoose.set('useFindAndModify', false);
