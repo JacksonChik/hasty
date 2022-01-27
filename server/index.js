b8b6d54b9c61361955875b43471a228152d020f6
Chik Jackson <Jacksonchik01@hotmail.com>
2022-01-27 02:10:39 -0800

    Installed dotenv to load env variables

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
+// loads environment variables from a .env file into process.env
 
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
