56a863ab5de9d8f3170851c2abb93820148dda68
Chik Jackson <Jacksonchik01@hotmail.com>
2022-02-02 08:25:55 -0800

    Added line chart component and the its frame. More to come

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { Container, AppBar,  Typography, Grow, Grid} from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
-import { getPosts } from './actions/posts';
+import { getPosts } from "./actions/posts";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import useStyles from './styles';
+import useStyles from "./styles";
 import LineChart from "./components/Charts/LineChart/LineChart";
 
-const App = () =>{
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const posts = useSelector((state) => state.posts);
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
 
-    useEffect(() => {
-        dispatch(getPosts());
-    },[dispatch])
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
-    return (
-        <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography variant="h4" align="center">
-                    Welcome back! You have applied to {posts? posts.length : 0} jobs in total this season.
-                </Typography>
-            </AppBar>
-            <LineChart height={200} width={500} data={[]}/>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+  return (
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h4" align="center">
+          Welcome back! You have applied to {posts ? posts.length : 0} jobs in
+          total this season.
+        </Typography>
+      </AppBar>
+      <LineChart height={200} width={1000} data={[]} />
+      {/* data should be an array of objects, where every object refers to the job search stats(only job count for now) of 1 day! */}
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
         </Container>
-    )
-}
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
--- client/src/components/Charts/LineChart/LineChart.js
+++ client/src/components/Charts/LineChart/LineChart.js
@@ -1,21 +1,86 @@
-import React, {useEffect} from 'react'
-import * as d3 from 'd3'
+import React, { useEffect } from "react";
+import * as d3 from "d3";
 
-import './index.css'
+import "./index.css";
+
+// TODO : make the graph responsive
+// https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
 
 const LineChart = (props) => {
-    const { data, width, height } = props;
-    
-    const drawChart = () => {
-        // TO CHANGE
-    }
+  const { data, height, width } = props;
 
-    useEffect(() => {
-      drawChart();
-    }, [data]);
-    
-    
-    return <div id="container" />;
-  }
-  
-  export default LineChart;
+  const drawChart = () => {
+    // TO CHANGE
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 30, left: 30 };
+
+    const svg = d3
+      .select("#container")
+      .append("svg")
+      .attr("height", height)
+      .attr("width", width)
+      .append("g");
+    // .attr("transform", `translate(${margin.left},${margin.top})`);
+    svg
+      .append("rect")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("fill", "silver");
+    const xScale = d3
+      .scaleLinear()
+      .domain([
+        d3.min(data, (data) => data.date),
+        d3.max(data, (data) => data.date),
+      ])
+      .range([0, width]);
+    // This specifies where(and what value) the x-axis starts and ends. It should be based the date of the job application stats(x-axis)
+    // count refers to the total number of job applications sent per day
+    // d3.min and d3.max return the max and min value in an array!(it is basically an iterator) The second argument is an accessor to retrieve the specific property
+    // to compare...
+    svg
+      .append("g")
+      .attr("class", "grid")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(xScale).tickSize(-height).tickFormat(""));
+    svg
+      .append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom().scale(xScale).tickSize(15));
+
+    const yScale = d3
+      .scaleLinear()
+      .range([height, 0])
+      .domain([0, d3.max(data, (data) => data.value)]);
+
+    svg
+      .append("g")
+      .attr("class", "grid")
+      .call(d3.axisLeft(yScale).tickSize(-width).tickFormat(""));
+
+    svg.append("g").attr("class", "y-axis").call(d3.axisLeft(yScale));
+
+    const line = d3
+      .line()
+      .x((d) => xScale(d.date))
+      .y((d) => yScale(d.count))
+      .curve(d3.curveMonotoneX);
+    // d.date and d. count?
+    svg
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "#f6c3d0")
+      .attr("stroke-width", 4)
+      .attr("class", "line")
+      .attr("d", line);
+  };
+
+  useEffect(() => {
+    drawChart();
+  }, [data]);
+
+  return <div id="container" />;
+};
+
+export default LineChart;
